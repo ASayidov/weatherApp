1e549ffa8ac4c5402897860daf3ed4ca1909143a
ASayidov <abdullajonsayidov@gmail.com>
2023-03-24 21:37:33 +0500

    added forcqst js

--- js/forecast.js
+++ js/forecast.js
@@ -1,4 +1,19 @@
-const KEY = '96b947a45d33d7dc1c49af3203966408'
-// bu mening kalitim. Agar o'zingiz olsangiz va ishlamasa buni ishlating.
-// Agar ishlamay qolsa unda 1 soatda so'rovlar soni limitdan oshib ketgan bo'ladi.
-// Aloqa: Telegram => @akror_web
+let apiKey = '80326728f693fadf142098e55e95a622';
+let cityName = 'Uzbekistan';
+
+let API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+
+const getData = async (resurs) => {
+
+  const request = await fetch(resurs);
+  if (request.status != 200) {
+    throw new Error('Xatolik yuz berdi')
+  }
+  const response = await request.json();
+
+  return response
+}
+
+getData(API).then((data) => {
+  console.log(data);
+}).catch((err) => { console.log(err.message) })
