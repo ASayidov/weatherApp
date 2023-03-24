984cfc67a4b464e035f6bd0c2c0714eac9594eb3
ASayidov <abdullajonsayidov@gmail.com>
2023-03-24 21:37:07 +0500

    changed api keys

--- js/app.js
+++ js/app.js
@@ -3,7 +3,3 @@ const card = document.getElementById('card')
 const details = document.getElementById('details')
 const weatherIcon = document.getElementById('weather-icon')
 const overlay = document.getElementById('overlay')
-
-let apiKey = '80326728f693fadf142098e55e95a622'
-
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={apiKey}
