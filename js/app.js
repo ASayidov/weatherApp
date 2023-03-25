f3e544006347420555fbd847a338e3c29f28b17b
ASayidov <abdullajonsayidov@gmail.com>
2023-03-25 13:45:20 +0500

    added func update UI

--- js/app.js
+++ js/app.js
@@ -6,18 +6,31 @@ const overlay = document.getElementById('overlay')
 const input = document.getElementById('city')
 
 
-//get weather
+//update ui
 
-const getWeather = async (city) => {
-  const reqWeather = await getData(city)
-  console.log(reqWeather);
+const updateUI = (weather) => {
+  // console.log(weather);
+  details.innerHTML = `
+  <h5 class="mb-3">${weather.name}, ${weather.sys.country}</h5>
+          <p class="mb-3">${weather.weather[0].main}</p>
+          <div class="display-4 mb-3">
+            <span>${Math.round(weather.main.temp)}</span>
+            <span>&deg;C</span>
+  `
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none')
+  }
+  weatherIcon.src = `
+  https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
 }
-
 
 //get location
 changeLocation.addEventListener('submit', (e) => {
   e.preventDefault()
   const cityName = input.value.trim()
   changeLocation.reset()
-  getWeather(cityName)
+  getData(cityName).then((data) => {
+    updateUI(data)
+  })
+
 })
