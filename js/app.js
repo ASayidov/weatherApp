fa0e31a5f525c06c439dd078839354e751cda29f
ASayidov <abdullajonsayidov@gmail.com>
2023-03-25 11:44:43 +0500

    added func getWeather

--- js/app.js
+++ js/app.js
@@ -5,10 +5,19 @@ const weatherIcon = document.getElementById('weather-icon')
 const overlay = document.getElementById('overlay')
 const input = document.getElementById('city')
 
+
+//get weather
+
+const getWeather = async (city) => {
+  const reqWeather = await getData(city)
+  console.log(reqWeather);
+}
+
+
 //get location
 changeLocation.addEventListener('submit', (e) => {
   e.preventDefault()
   const cityName = input.value.trim()
-  console.log(cityName);
   changeLocation.reset()
+  getWeather(cityName)
 })
